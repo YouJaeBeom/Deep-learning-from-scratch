905a766bfe7b7fcba9fe87eadbba967b2f03451c
youjaebeom <dbwo4011@gmail.com>
2020-07-13 01:03:05 +0900

    간단한 신경망 구축 활성화 함수 sigmoid , relu 출력층 identity , softmax

--- SimpleLayer.py
+++ SimpleLayer.py
@@ -15,6 +15,22 @@ def identity_function(x):
     return x
 
 # 2. softmax function -> 분류
+# overflow 발생
+def softmax(x):
+    exp_a=np.exp(x)
+    sum_exp_a=np.sum(exp_a)
+    y=exp_a/sum_exp_a
+    return y
+# overflow 문제로 수정
+def softmax_modify(x):
+    c=np.max(x)
+    exp_a=np.exp(x-c)
+    sum_exp_a=np.sum(exp_a)
+    y=exp_a/sum_exp_a
+    return y
+
+
+
 def init_network():
     network={}
     network['W1']=np.array([[0.1,0.3,0.5],[0.2,0.4,0.6]])
@@ -54,9 +70,41 @@ def forward2(network,x):
 
     return y
 
+## 활성화 함수 sigmoid + softmax
+def forward3(network,x):
+    W1,W2,W3=network['W1'],network['W2'],network['W3']
+    b1,b2,b3=network['b1'],network['b2'],network['b3']
+
+    a1 = np.dot(x,W1)+b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1,W2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2, W3) + b3
+    y=softmax_modify(a3)
+
+    return y
+
+## 활성화 함수 relu + softmax
+def forward4(network,x):
+    W1,W2,W3=network['W1'],network['W2'],network['W3']
+    b1,b2,b3=network['b1'],network['b2'],network['b3']
+
+    a1 = np.dot(x,W1)+b1
+    z1 = relu(a1)
+    a2 = np.dot(z1,W2) + b2
+    z2 = relu(a2)
+    a3 = np.dot(z2, W3) + b3
+    y=softmax_modify(a3)
+
+    return y
+
 network=init_network()
 x=np.array([1.0,0.5])
 y1=forward1(network,x)
-print(y1)
+print("sigmoid + identity",y1)
 y2=forward2(network,x)
-print(y2)
+print("relu + identity",y2)
+y3=forward3(network,x)
+print("sigmoid + softmax",y3)
+y4=forward4(network,x)
+print("relu + softmax",y4)
